87e70a8735ac3ef66342b149fc25c6fdf90eecef
Berdnik <ggwpez1987@mail.ru>
2023-11-02 00:41:14 +0700

    small changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import cn from 'classnames';
+import './styles/App.css'
 
-function App() {
+function App(props) {
+  const initData = [...props.items]
+  const [state, sortItems] = useState(initData);
+
+  const sortByInc = () => {
+    const sorted = initData.sort((item2, item1) => item1.price > item2.price ? 1 : -1);
+    sortItems(sorted);
+  }
+  const sortByDec = () => {
+    const sorted = initData.sort((item2, item1) => item1.price > item2.price ? 1 : -1);
+    sortItems(sorted);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <table>
+        <tr>
+          <td className="titles">Name</td>
+          <td className="titles">Description</td>
+          <td className="titles">Price</td>
+          <td className="titles">Rating</td>
+        </tr>
+        {state.map((item) => {
+          const itemPrice = item.price;
+          return <tr className={cn({
+            "red": itemPrice > 90,
+            "green": itemPrice < 50,
+            "yellow": itemPrice > 50 && itemPrice < 90,
+          })}>
+            <td>{item.name}</td>
+            <td>{item.description}</td>
+            <td>{item.price}</td>
+            <td>{item.rating}</td>
+          </tr>
+        })}
+      </table>
+      <div className="btn-group">
+      <button className='button' onClick={sortByInc}>Сортировать по возрастанию цены</button>
+      <button className='button' onClick={sortByDec}>Сортировать по убыванию цены</button>
     </div>
-  );
+    </div>
+  )
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,62 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-
+const items = [
+  {
+    name: 'Head & Shoulders',
+    description: 'Мужской шампунь 100 в 1',
+    price: '70',
+    rating: '5',
+  },
+  {
+    name: 'Голова & плечи',
+    description: 'Результат программы импортозамещения',
+    price: '49',
+    rating: '5',
+  },
+  {
+    name: 'Nivea men',
+    description: 'Мужской шампунь ',
+    price: '100',
+    rating: '4',
+  },
+  {
+    name: 'Axe',
+    description: 'Гель для душа',
+    price: '30',
+    rating: '3',
+  },
+  {
+    name: 'Тоже шампунь',
+    description: 'Тоже шампунь 2 в 1',
+    price: '95',
+    rating: '4',
+  },
+  {
+    name: 'Clear men',
+    description: 'Никто такое не покупает',
+    price: '45',
+    rating: '4',
+  },
+  {
+    name: 'Kapous Professional',
+    description: 'Вроде профессиональный шампунь',
+    price: '110',
+    rating: '3',
+  },
+  {
+    name: 'KeraSays',
+    description: 'Кера говорит',
+    price: '20',
+    rating: '1',
+  },
+  {
+    name: 'Хозяйственное мыло',
+    description: 'Кожа скажет спасибо',
+    price: '1',
+    rating: '5',
+  },
+]
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(<App items={items}/>);
